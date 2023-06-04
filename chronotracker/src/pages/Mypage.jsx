06b751d4df4e1fa537397ff4117a5a61010fecc8
thatkhay <johnkingsleyegeonu@gmail.com>
2023-06-04 13:04:37 +0100

    boxes

--- chronotracker/src/pages/Mypage.jsx
+++ chronotracker/src/pages/Mypage.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Card from '../component/Card'
-
-
+import Myimg from '../img/image-jeremy.png'
 
 function Mypage() {
   return (
     <div className='container'>
     <div className='biocontainer'>
     <div className='bio' > 
-    <img alt=''/> 
-    <span>Report for</span>
+    <img className='img' src={Myimg} alt='bio-img'/> 
+    <span className='span'>Report for</span>
     <h1>Jeremy Robson</h1>
     </div>
     <div className='list'>
@@ -20,11 +19,14 @@ function Mypage() {
       </ul>
     </div>
     </div>
+    <div className='mainBoxContainer'>
    <Card text='23hrs' color='blue' />
    <Card text='50hrs' color='red' />
    <Card text='60hrs' color='violet' />
    <Card text='10hrs' color='green' />
    <Card text='14hrs' color='brown' />
+   <Card text='14hrs' color='pink' />
+   </div>
     </div>
   )
 }
